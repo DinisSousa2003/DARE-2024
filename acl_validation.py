7e9ce463d6f18d93fc0507c5192be866e85a51f4
DinisSousa2003 <dinis.bessa.sousa@gmail.com>
2024-09-20 19:39:16 +0100

    Authority graph

--- acl_validation.py
+++ acl_validation.py
@@ -176,3 +176,58 @@ def computeSeniority(ops):
     
     ops_by_pk = {pk: a.get(pk, {}).union(op) for (pk, op) in added}
     return {pk: min(ops, key=lambda op: (depth[op], hex_hash(op))) for (pk, ops) in ops_by_pk}
+
+def subject(op):
+    """
+    Returns the subject of an operation.
+    """
+    return op["signed_by"]
+
+def authorityGraph(ops):
+    """
+    Returns the authority graph of a set of operations.
+
+    Args:
+        ops (set): a set of operations
+
+    Returns:
+        authorityGraph: the authority graph of the operations, 
+        represented as a set of edges.
+        There is an edge from op1 to op2 in this graph if operation 
+        op1 may affect whether op2 is authorized.
+
+        More precisely, there is an edge from op1 to op2 if:
+            1) op1 is an operation that adds the device with public
+            key pk (or op1 is the initial group creation by device pk), and op2
+            is a causally succeeding operation performed by device pk; or
+            2) op1 is an operation that removes the device with
+            public key pk, op2 is an operation performed by
+            device pk, and op1 either precedes op2 or the two operations are concurrent.
+
+    3) we also add a vertex (member, pk) for the public key 
+    pk of every device that was ever mentioned in an operation, 
+    and we add an edge to that vertex from each operation that may 
+    affect the permissions associated with pk
+    """
+    ops_by_hash = {hex_hash(op): verify_msg(op) for op in ops}
+    authGraph = set()
+    for op2 in ops:
+        pk = op2["signed_by"]
+        for op1 in ops:
+            #1)
+            if ((op1["type"] == "create" and op1["signed_by"] == pk
+                or op1["type"] == "add" and op1["added_key"] == pk)
+                and precedes(ops_by_hash, op1, op2)):
+                authGraph.add((op1, op2))
+            #2)
+            if (op1["type"] == "remove" and op1["removed_key"] == pk
+                and not precedes(ops_by_hash, op1, op2)):
+                authGraph.add((op1, op2))
+        #3)
+        if (op2["type"] == "create"):
+            authGraph.add((op2, ("member", pk)))
+        elif (op2["type"] == "add"):
+            authGraph.add((op2, ("member", op2["added_key"])))
+        elif (op2["type"] == "remove"):
+            authGraph.add((op2, ("member", op2["removed_key"])))        
+
